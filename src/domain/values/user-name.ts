1980e5171fd9063deb92152b37b9164132a86491
Duino-nano <wsdqr907869@yahoo.co.jp>
2023-07-11 21:52:56 +0900

    user-name.tsを追加

---
+++ src/domain/values/user-name.ts
@@ -0,0 +1,12 @@
+export class UserName {
+    constructor(readonly value: string) {
+        this.validation(value)
+    }
+
+    private validation(value: unknown) {
+        const isValid = typeof value === 'string' && value.length >= 1;
+        if (!isValid) {
+            throw new Error(`ユーザー名は1文字以上の文字列である必要があります`)
+        }
+    }
+}
